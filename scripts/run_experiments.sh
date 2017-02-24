9dbf842ed80fd80c0527e6634df8cfcc7dc889ba
Simon Brodeur <Simon Brodeur@USherbrooke.ca>
2017-02-24 10:20:05 -0500

    Fix commented commands in script

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -8,11 +8,11 @@ PROJECT_ROOT=$( cd "$( dirname "${DIR}/../.." )" && pwd )
 export PYTHONPATH=$PROJECT_ROOT:$PYTHONPATH
 
 # Clean results
-#$DIR/clean.sh
+$DIR/clean.sh
 
 # Generate datasets and get results for dataset-simple
-#python $DIR/generate_dataset_toy.py 2>&1 | tee -a generate_dataset_toy.log
-#python $DIR/scale_weight_effect_mlcsc_toy.py 2>&1 | tee -a scale_weight_effect_mlcsc_toy.log
+python $DIR/generate_dataset_toy.py 2>&1 | tee -a generate_dataset_toy.log
+python $DIR/scale_weight_effect_mlcsc_toy.py 2>&1 | tee -a scale_weight_effect_mlcsc_toy.log
 
 # Generate datasets and get results for dataset-complex
 python $DIR/generate_dataset.py 2>&1 | tee -a generate_dataset.log
